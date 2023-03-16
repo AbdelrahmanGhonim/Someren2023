9cea9cd5d69c2b59cb581dae3c634df068e860a7
WKolasaa <wojtusk574@gmail.com>
2023-03-16 14:58:54 +0100

    Update

---
+++ SomerenDAL/LecturerDAO.cs
@@ -0,0 +1,38 @@
+﻿using SomerenModel;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomerenDAL
+{
+    public class LecturerDAO : BaseDao
+    {
+        public List<Teacher> GetAllTeachers()
+        {
+            string query = "SELECT id, name, lecturerNumber, dataOfBirth FROM Lecturers";
+            SqlParameter[] sqlParameters = new SqlParameter[0];
+            return ReadTables(ExecuteSelectQuery(query, sqlParameters));
+        }
+        private List<Teacher> ReadTables(DataTable dataTable)
+        {
+            List<Teacher> teachers = new List<Teacher>();
+
+            foreach (DataRow dr in dataTable.Rows)
+            {
+                Teacher teacher = new Teacher()
+                {
+                    Id = (int)dr["id"],
+                    Name = dr["Name"].ToString(),
+                    Number = (int)dr["lecturerNumber"],
+                    DateOfBirth = (DateTime)dr["DataOfBirth"]
+                };
+                teachers.Add(teacher);
+            }
+            return teachers;
+        }
+    }
+}
---
+++ SomerenService/TeacherService.cs
@@ -0,0 +1,22 @@
+﻿using SomerenDAL;
+using SomerenModel;
+using System.Collections.Generic;
+
+namespace SomerenService
+{
+    public class TeacherService
+    {
+        private LecturerDAO lecturerdb;
+
+        public TeacherService()
+        {
+            lecturerdb = new LecturerDAO();
+        }
+
+        public List<Teacher> GetLecturers()
+        {
+            List<Teacher> teachers = lecturerdb.GetAllTeachers();
+            return teachers;
+        }
+    }
+}
