7195345aa9136c14b42825b1def5c5caeb76cc79
Abdelrahman a Ghonim <695857@student.inholland.nl>
2023-03-26 03:52:23 +0200

    almost done

---
+++ SomerenService/ActivityService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SomerenModel;
+using SomerenDAL;
+
+namespace SomerenService
+{
+    public class ActivityService
+    {
+        private ActivitiesDao activitiesdb;
+        public ActivityService()
+        {
+            activitiesdb = new ActivitiesDao(); 
+        }
+        public List<Activities> GetActivities()
+        {
+            List<Activities> activities=activitiesdb.GetAllActivities();
+            return activities;
+        }
+        public void AddActivity(Activities activities)
+        {
+            activitiesdb.AddActivity(activities);
+        }
+        public void UpdateActivity(Activities activities)
+        {
+            activitiesdb.UpdateActivity(activities);
+        }
+        public void RemoveActivity(Activities activities)
+        {
+            activitiesdb.RemoveActivity(activities);
+        }
+    }
+}
